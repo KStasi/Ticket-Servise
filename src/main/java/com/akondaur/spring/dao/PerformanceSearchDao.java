657af8d439d6c59d946f874753e79ca276800b75
kstasi <nastasiakondaurova@gmail.com>
2019-11-09 20:55:38 +0200

    performance search dao added

---
+++ src/main/java/com/akondaur/spring/dao/PerformanceSearchDao.java
@@ -0,0 +1,41 @@
+package com.akondaur.spring.dao;
+
+import com.akondaur.model.Performance;
+import com.akondaur.model.PerformanceMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Component
+public class PerformanceSearchDao {
+    JdbcTemplate jdbcTemplate;
+
+    private final String SQL_GET_BY_TYPE = "select * from performances where type = ?";
+    private final String SQL_GET_BY_THEATRE = "select * from performances where theatre like ?";
+    private final String SQL_GET_ALL = "select * from performances";
+    private final String SQL_GET_BY_NAME = "select * from performances where name like ?";
+
+    @Autowired
+    public PerformanceSearchDao(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public List<Performance> getAll() {
+        return jdbcTemplate.query(SQL_GET_ALL, new PerformanceMapper());
+    }
+
+    public List<Performance> getByType(Integer type) {
+        return jdbcTemplate.query(SQL_GET_BY_TYPE, new Object[] { type }, new PerformanceMapper());
+    }
+
+    public List<Performance> getByTheatre(String theatre) {
+        return jdbcTemplate.query(SQL_GET_BY_THEATRE, new Object[] { theatre }, new PerformanceMapper());
+    }
+
+    public List<Performance> getByName(String name) {
+        return jdbcTemplate.query(SQL_GET_BY_NAME, new Object[] { name }, new PerformanceMapper());
+    }
+}
