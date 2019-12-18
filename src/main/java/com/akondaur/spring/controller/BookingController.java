d5427531da5c1a1128bfb7d6b696cac4fa5e6555
kstasi <nastasiakondaurova@gmail.com>
2019-12-18 06:29:43 +0200

    client added

--- src/main/java/com/akondaur/spring/controller/BookingController.java
+++ src/main/java/com/akondaur/spring/controller/BookingController.java
@@ -13,30 +13,30 @@ public class BookingController {
     @Autowired
     BookingRepository bookingRepository;
 
-    // @GetMapping("")
-    // public Iterable<Booking> getBookingAll() {
-    //     return bookingRepository.findAll();
-    // }
-    // 
-    // @PostMapping("")
-    // public Booking createBooking(@RequestBody
-    //         @RequestParam(value = "id") Long id,
-    //         @RequestParam(value = "clientId") Long clientId,
-    //         @RequestParam(value = "ticketId") Long ticketId,
-    //         @RequestParam(value = "price") Integer price
-    //     ) {
-    //     Booking booking = new Booking();
-    //     booking.setId(id);
-    //     booking.setClientId(clientId);
-    //     booking.setTicketId(ticketId);
-    //     booking.setPrice(price);
-    // 
-    //     bookingRepository.save(booking);
-    //     return booking;
-    // }
-    // 
-    // @GetMapping("{id}")
-    // public Optional<Booking> getBooking(@PathVariable Long id) {
-    //     return bookingRepository.findById(id);
-    // }
+    @GetMapping("")
+    public Iterable<Booking> getBookingAll() {
+        return bookingRepository.findAll();
+    }
+    
+    @PostMapping("")
+    public Booking createBooking(@RequestBody
+            @RequestParam(value = "id") Long id,
+            @RequestParam(value = "clientId") Long clientId,
+            @RequestParam(value = "ticketId") Long ticketId,
+            @RequestParam(value = "price") Integer price
+        ) {
+        Booking booking = new Booking();
+        booking.setId(id);
+        booking.setClientId(clientId);
+        booking.setTicketId(ticketId);
+        booking.setPrice(price);
+    
+        bookingRepository.save(booking);
+        return booking;
+    }
+    
+    @GetMapping("{id}")
+    public Optional<Booking> getBooking(@PathVariable Long id) {
+        return bookingRepository.findById(id);
+    }
 }
---
+++ src/main/java/com/akondaur/spring/controller/ClientsController.java
@@ -0,0 +1,44 @@
+package com.akondaur.spring.controller;
+import java.lang.Iterable;
+import java.util.Optional;
+
+import com.akondaur.model.Client;
+import com.akondaur.spring.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientsController {
+    @Autowired
+    ClientRepository clientRepository;
+
+    @GetMapping("")
+    public Iterable<Client> getBookingAll() {
+        return clientRepository.findAll();
+    }
+    
+    @PostMapping("")
+    public Client createBooking(@RequestBody
+            @RequestParam(value = "id") Long id,
+            @RequestParam(value = "firstName") String firstName,
+            @RequestParam(value = "lastName") String lastName,
+            @RequestParam(value = "price") Double discount,
+            @RequestParam(value = "email") String email
+        ) {
+        Client client = new Client();
+        client.setId(id);
+        client.setFirstName(firstName);
+        client.setLastName(lastName);
+        client.setDiscount(discount);
+        client.setEmail(email);
+    
+        clientRepository.save(client);
+        return client;
+    }
+    
+    @GetMapping("{id}")
+    public Optional<Client> getBooking(@PathVariable Long id) {
+        return clientRepository.findById(id);
+    }
+}
